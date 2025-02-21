238a304a52b6c22e0a07f070d97a7124e1774b71
Max Koon <theakman96@gmail.com>
2025-02-21 12:44:08 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -91,7 +91,7 @@ function App() {
         isOpen={isGameLost}
       />
       <div className="flex w-80 mx-auto items-center mb-8">
-        <h1 className="text-xl grow font-bold">BURGLE</h1>
+        <h1 className="text-xl grow font-bold">BURGLE 🍔</h1>
         <InformationCircleIcon
           className="h-6 w-6 cursor-pointer"
           onClick={() => setIsInfoModalOpen(true)}
